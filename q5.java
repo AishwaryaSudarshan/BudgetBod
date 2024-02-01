94f6ed782e61450ad5036db1b8c38e3eb57297f3
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-02-01 11:02:33 -0600

    create a

--- q5.java
+++ q5.java
@@ -1,5 +1,6 @@
 public class q5 {
         public int strStr(String haystack, String needle) {
+        int a = 0;
          if (needle.length() == 0)
              return 0;
          return haystack.indexOf(needle);
