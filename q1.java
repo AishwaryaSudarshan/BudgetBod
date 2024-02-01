7af2f1ad069258d746aa5e50675392e0507fa04d
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-02-01 11:18:04 -0600

    update q1 with comments

--- q1.java
+++ q1.java
@@ -2,12 +2,18 @@ import java.util.*;
 
 class q1 {
     public int[] twoSum(int[] nums, int target) {
+        //create an array of 2
         int[] arr = new int[2];
+
+        //creat a hashmap of key numbers
         HashMap<Integer, Integer> numKey = new HashMap<>();
+        
+        //add to hashmap
         for (int i = 0; i < nums.length; i++) {
             numKey.put(nums[i], i);
         }
 
+        //go through hash map
         for (int key : numKey.keySet())
         {
             int secondValue = target - key;
@@ -15,6 +21,7 @@ class q1 {
             {
                 if (numKey.get(secondValue) != numKey.get(key))
                 {
+                    //change the value in arrays
                     arr[0] = numKey.get(key);
                     arr[1] = numKey.get(secondValue);
                     break;
@@ -23,6 +30,7 @@ class q1 {
                     continue;
             }
         }
+        //return array
         return arr;
 
     }
