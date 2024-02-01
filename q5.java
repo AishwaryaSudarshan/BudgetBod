7fa4dad0fde5361e1f76a1637d8366ecce071695
Allen-95 <ajpaul750@gmail.com>
2024-02-01 11:15:55 -0600

    Updated Q5
    added 2 variables.

--- q5.java
+++ q5.java
@@ -1,6 +1,8 @@
 public class q5 {
         public int strStr(String haystack, String needle) {
         int a = 0;
+        int w = 3;
+        int s = 4;
          if (needle.length() == 0)
              return 0;
          return haystack.indexOf(needle);
