4f0f9dee0a6e41ec7a28f67c3f6d961747bf9f0d
Allen-95 <ajpaul750@gmail.com>
2024-03-21 11:24:16 -0500

    Create sign in page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Chat from "../src/components/Chat";
+import SignInSide from "./components/signin";
 import './App.css';
 
 function App() {
   return (
     <div className="">
       <Chat />
+      <SignInSide />
     </div>
   );
 }
