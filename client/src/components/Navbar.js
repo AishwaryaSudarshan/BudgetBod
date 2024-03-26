5be9be22199e278bb7e204774257690b83f99a40
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-03-26 10:53:30 -0500

    update frontend packaging plus edits

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from 'react';
+import '../App.css';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div className="">
-        
-    </div>
+    <nav className="bg-gray-100 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center space-x-4">
+          {/* Assuming you have SVG icons or images for these */}
+          <a href="/" className="font-semibold text-gray-800 hover:text-gray-600 transition duration-300">Homepage</a>
+          <a href="/recipes" className="font-semibold text-gray-800 hover:text-gray-600 transition duration-300">Recipes</a>
+          <a href="/dashboard" className="font-semibold text-gray-800 hover:text-gray-600 transition duration-300">Dashboard</a>
+          <a href="/settings" className="font-semibold text-gray-800 hover:text-gray-600 transition duration-300">Settings</a>
+        </div>
+        {/* <div className="flex items-center space-x-4">
+          
+          <button className="bg-green-500 text-white px-4 py-2 rounded shadow hover:bg-green-400 transition duration-300">Logout</button>
+        </div> */}
+      </div>
+    </nav>
   );
-}
+};
 
 export default Navbar;
--- client/src/components/signin.js
+++ client/src/components/signin.js
@@ -12,13 +12,14 @@ import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Background from '../images/budgetbodwallpaper.jpeg';
 
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        BudgetBod
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -50,7 +51,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundImage: Background,
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
