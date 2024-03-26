5cf9e76e2b1368efd0c4d8d1fd9ee70994cda909
AishwaryaSudarshan <aishwaryasudarshan18@gmail.com>
2024-03-26 10:53:03 -0500

    create switch pages use case functional

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">Homepage</h1>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ client/src/pages/Recipes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Recipes() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">Recipes</h1>
+    </div>
+  );
+}
+
+export default Recipes;
